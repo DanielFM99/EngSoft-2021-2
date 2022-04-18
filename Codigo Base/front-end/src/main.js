7984aa5143b157315085f32459919229c8c792ca
IgorFreiredeMorais <igorfreire110@gmail.com>
2022-04-18 18:18:19 -0300

    closes #28; Adiciona configuracoes

--- Codigo Base/front-end/src/main.js
+++ Codigo Base/front-end/src/main.js
@@ -2,12 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueMask from "v-mask";
+import Vuelidate from "vuelidate";
 
 import { BootstrapVue } from "bootstrap-vue";
-// import { IconsPlugin } from 'bootstrap-vue'
+import { IconsPlugin } from "bootstrap-vue";
 
 Vue.use(BootstrapVue);
-// Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
+Vue.use(VueMask);
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
 import "bootstrap/dist/css/bootstrap.css";
@@ -16,5 +20,5 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
