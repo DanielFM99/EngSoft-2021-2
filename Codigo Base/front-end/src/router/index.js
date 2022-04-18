ecf9eb6c4c47b82153c66d5094837709ecfa8b90
IgorFreiredeMorais <igorfreire110@gmail.com>
2022-04-18 18:16:32 -0300

    closes #27; Adiciona router

--- Codigo Base/front-end/src/router/index.js
+++ Codigo Base/front-end/src/router/index.js
@@ -7,8 +7,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: () =>
-      import(/* webpackChunkName: "home" */ "../views/Home.vue")
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
   },
   {
     path: "/about",
@@ -17,14 +16,76 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/cliente/adicionar",
+    name: "adicionarCliente",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../components/adicionarCliente.vue"
+      ),
+  },
+  {
+    path: "/funcionario/adicionar",
+    name: "adicionarFuncionario",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../components/adicionarFuncionario.vue"
+      ),
+  },
+  {
+    path: "/vendas/adicionar",
+    name: "adicionarVendas",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../components/adicionarVendas.vue"
+      ),
+  },
+  {
+    path: "/clientes/editar/:id",
+    name: "editarClientes",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../components/editarCliente.vue"),
+  },
+  {
+    path: "/funcionarios/editar/:id",
+    name: "editarFuncionarios",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../components/editarFuncionario.vue"
+      ),
+  },
+  {
+    path: "/vendas/editar/:id",
+    name: "editarVendas",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../components/editarVendas.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
